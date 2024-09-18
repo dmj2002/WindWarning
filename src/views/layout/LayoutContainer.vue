bd21d3f0a0e7e65503631e327aafe6ede6ed2855
Ding mengjie <D=473332451@qq.com>
2024-09-18 12:36:49 +0800

    完成导航栏

--- src/views/layout/LayoutContainer.vue
+++ src/views/layout/LayoutContainer.vue
@@ -1,7 +1,7 @@
 <script setup>
 import 'element-plus/dist/index.css'
 import { useRoute } from 'vue-router'
-
+import { User } from '@element-plus/icons-vue'
 const route = useRoute()
 </script>
 
@@ -13,24 +13,64 @@ const route = useRoute()
     active-text-color="#ffd04b"
     :default-active="route.path"
   >
-    <el-menu-item index="1">
+    <h2 class="title">
       <span>风电预警系统</span>
-    </el-menu-item>
+    </h2>
     <el-menu-item index="/home">首页</el-menu-item>
     <el-menu-item index="/warning">预警</el-menu-item>
     <el-menu-item index="/report">报告</el-menu-item>
     <el-menu-item index="/settings">设置</el-menu-item>
+    <div class="user-avatar">
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          <!-- <el-avatar icon="el-icon-user" class="custom-avatar" /> -->
+          <el-avatar class="custom-avatar">
+            <el-icon><User /></el-icon>
+          </el-avatar>
+        </span>
+        <template v-slot:dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item @click="logout">退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
   </el-menu>
   <router-view></router-view>
 </template>
 
 <style scoped>
+.title {
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+  text-align: center;
+  margin: 20px 0;
+  padding: 10px;
+  background-color: #545c64;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.title span {
+  display: inline-block;
+  padding: 0 10px;
+}
 .el-menu {
   display: flex;
   justify-content: flex-start;
   align-items: center;
 }
-.last-item {
-  margin-left: auto;
+.user-avatar {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+}
+
+.el-dropdown-link {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  outline: none;
 }
 </style>
